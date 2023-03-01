6be4547aaf680c213630a67e1ecfd448edde4356
Your Name <you@example.com>
2023-03-01 17:23:44 +0100

    fatto

--- dispari/js/main.js
+++ dispari/js/main.js
@@ -1,15 +1,57 @@
 "use strict";
 
+const pcChoice = Math.floor(Math.random() * 5) + 1;
+console.log(pcChoice);
 const userChoice = prompt ("Ciao! Benvenuto nel simulatore di pari e dispari. Pari o Dispari?", "" );
-console.log(userChoice)
+console.log(userChoice);
 
 
-if (userChoice == "Pari", "pari"){
+
+if (userChoice == "Pari" || userChoice == "pari"){
     let userNum = Number(prompt("Inserisci un numero che va da 1 a 5", " "));
+
+    if (userNum > 5) {
+        alert("Non hai inserito un valore corretto. Ricarica la pagina per giocare di nuovo");
+    }
+
+    else {
+        function sommaNumeri (){
+            const somma = userNum + pcChoice;
+            console.log(somma)
+            if (somma % 2 == 0) {
+                alert("You win!");
+            }
+            else {
+                alert("PC win!");
+            }
+            return somma;
+        }
+        sommaNumeri();
+    }
+    
 }
 
-else if (userChoice == "Dispari", "dispari"){
+else if (userChoice == "Dispari" || userChoice == "dispari"){
     let userNum = Number(prompt("Inserisci un numero che va da 1 a 5", " "));
+
+    if (userNum > 5) {
+        alert("Non hai inserito un valore corretto. Ricarica la pagina per giocare di nuovo");
+    }
+
+    else {
+        function sommaNumeri (){
+            const somma = userNum + pcChoice;
+            console.log(somma)
+            if (somma % 2 == 0) {
+                alert("PC win!");
+            }
+            else {
+                alert("You win!");
+            }
+            return somma;
+        }
+        sommaNumeri();
+    }
 }
 
 else {
