82a5f922e6fa27a01816077e0738ccf889ab15eb
Your Name <you@example.com>
2023-03-01 16:39:07 +0100

    ticket

--- dispari/js/main.js
+++ dispari/js/main.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const userChoice = prompt ("Ciao! Benvenuto nel simulatore di pari e dispari. Pari o Dispari?", "" );
+console.log(userChoice)
+
+
+if (userChoice == "Pari", "pari"){
+    let userNum = Number(prompt("Inserisci un numero che va da 1 a 5", " "));
+}
+
+else if (userChoice == "Dispari", "dispari"){
+    let userNum = Number(prompt("Inserisci un numero che va da 1 a 5", " "));
+}
+
+else {
+    alert("Non hai inserito un valore corretto. Ricarica la pagina per giocare di nuovo")
+}
+
+/*let userNum = Number(prompt("Inserisci un numero che va da 1 a 5", " "));*/
+
+ 
+
+
+
+
+
+/*
+if(num % 2 == 0) {
+    alert("You win!");
+}
+else {
+    alert("PC win!");
+}
+*/
